076775a8225e395cfe1d75e6f149615c6b2ce6c6
FreyaBIT <cnfreyalee@foxmail.com>
2020-04-20 08:31:56 +0800

    Update mytime.cpp

--- processCtrl/linux/mytime.cpp
+++ processCtrl/linux/mytime.cpp
@@ -1,139 +1,50 @@
-/**
- *	Main Function: mytime
- * 
- *	Parameter: 
- *		1. A executable file path. ('./test' as default)
- *		2. Limit running time. (measured in sec.)
- *
- * 	Create a new process of given program name.
- *	Output its running time.
- *
- */
-
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/time.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <pthread.h>
-
-#define MAXN 128
-
-/*
- * parameters: 
- *		char *, a (absolute) path
- * return:
- *		char *, last part of path (exclude '/')
- * e.g:
- *		'/usr/boot/grub' -> 'grub'
- */
-char* getLastWord(char *path)
+/********************************************/
+/*名称：mytime.c
+/*描述：用命令行的形式建立一个新的进程，并保存其运行的时间 
+/*作者：野狼
+/*日期：2017－03－19
+/********************************************/
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/time.h>
+#include<sys/types.h>
+#include<unistd.h>
+ 
+int main(int argc, char **argv)
 {
-	char *p = path + strlen(path) - 1;
-	while (p > path && *p != '/' && *p != '\\') p--;
-	return (*p == '\\' || *p == '/') ? p+1 : p;
-}
-
-
-/*
- * Trasfer a str into a path
- * 
- * paramaters: 
- *		char *, src path
- *		char *, res path
- * e.g:
- *		'test' -> './test'
- *		'/usr/fred' -> '/usr/fred'
- */
-void proceedPath(char *path_temp, char *path)
-{
-	if (path_temp[0] != '\\' && path_temp[0] != '/' && path_temp[0] != '.') {
-		//strcpy(path, "./");
+	//调用系统时间
+	struct timeval time_start;
+	struct timeval time_end;
+	//用以记录进程运行的时间
+	float time_use = 0;
+	pid_t pid;
+	pid = fork();
+	if (pid < 0)	//如果出错
+	{
+		printf("Create Fail!");
+		exit(0);
 	}
-	strcat(path, path_temp);
-}
-
-void outputTimeUse(struct timeval * t1, struct timeval * t2)
-{
-	long long time_use = (t1->tv_sec - t2->tv_sec)*1000000 + (t1->tv_usec - t2->tv_usec);
-
-	printf("* %lld h %lld min %lld sec %lld ms %lld us. \n", 
-		(time_use) / 3600000000,
-		(time_use % 3600000000) / 60000000, 
-		(time_use % 60000000) / 1000000, 
-		(time_use % 1000000) / 1000, 
-		time_use % 1000
-	);
-}
-
-/* 
- *	Use multi-thread to check whether child process terminates in the given limit time.
- *	parameters:
- *		1.	void *, info including: int pid, int limit_time
- */
-void* waitThreadFunction(void * info)
-{
-	for (int i = 0; i < ((int *)info)[1]; i++) {
-		sleep(1);
+	else if (pid == 0)	//如果是子进程
+	{
+		printf("Create Child\n");
+		gettimeofday(&time_start,NULL);
+		printf("111time_start.tv_sec:%d\n",time_start.tv_sec);
+		printf("111time_start.tv_usec:%d\n\n",time_start.tv_usec);
+		//在子进程中调用execv函数在命令行中来运行一个程序
+		execv(argv[1],&argv[1]);
 	}
-	if (kill(((int *)info)[0], SIGKILL) != -1) {
-		printf("\n* Process was forced to terminate by watcher thread.\n");
-	}
-}
-
-int main(int argc, char *argv[])
-{
-	pid_t pid = vfork();
-	if (pid < 0) {
-		printf("Create process failed.\n");
-		return 0;
-	}
-	
-	char path_temp[MAXN] = "./test";
-	char path[MAXN] = "\0";
-	if (argc > 1) {	// else: use default "./test"
-		strcpy(path_temp, argv[1]);
-	}
-	proceedPath(path_temp, path);
-
-	struct timeval time_start, time_end;
-
-	// Child process.
-	if (pid == 0) {
-
-	// In vfork function, parent and child share these variables.
-	// Therefore we can use gettimeofday() here.
-		gettimeofday(&time_start, NULL);		
-
-	// Create process of given program.
-		execlp(path, getLastWord(path), (char *)0);
-	}
-
-	// Parents process.
-	else {
-		printf("* Process (%d) '%s' started. \n\n", pid, path);
-
-		if (argc > 2) {
-	// Use another watcher thread to terminate child process in certain time.
-	// Thread will force process to terminate by time. (print: 'forced'.)
-	// Else, ignore this thread, watcher will be killed when exit (main return).
-			int limit_time = atoi(argv[2]);
-			int pinfo[2] = {pid, limit_time};
-			pthread_t tid;
-
-			pthread_create(&tid, NULL, waitThreadFunction, pinfo);
-		}
-
-	// Wait for child process to terminate.
-		wait(0);
-		
-		gettimeofday(&time_end, NULL);
-
-		printf("\n* Process terminated.\n");
-		outputTimeUse(&time_end, &time_start);
-
+	else
+	{	
+		gettimeofday(&time_start,NULL);
+		printf("time_start.tv_sec:%d\n",time_start.tv_sec);
+		printf("time_start.tv_usec:%d\n\n",time_start.tv_usec);
+		wait(NULL);	//等待子进程结束
+		gettimeofday(&time_end,NULL);
+		printf("time_end.tv_sec:%d\n",time_end.tv_sec);
+		printf("time_end.tv_usec:%d\n",time_end.tv_usec);
+		time_use = (time_end.tv_sec - time_start.tv_sec)*1000000 + (time_end.tv_usec - time_start.tv_usec);
+		printf("此程序运行的时间为：%lf微秒",time_use);
 	}
 	return 0;
 }
